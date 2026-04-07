e5a26f474c679d2ea3a9584bfded2c93363563e8
Documenter.jl <documenter@juliadocs.github.io>
2026-04-07 16:33:54 +0000

    build based on df0f2fd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#Gustavo","page":"Home","text":"Documentation for Gustavo.\n\n","title":"Gustavo"},{"category":"module","location":"#Gustavo.Gustavo","page":"Home","text":"Only the best quality\n\n\n\n\n\n","title":"Gustavo.Gustavo"}]
+}
